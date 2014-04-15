b12c773155a470945232a5da2d66f09172a51ce4
ugho_49 <stephan.ugho@gmail.com>
2014-04-15 21:03:31 +0200

    FragmentAbout

--- ChevbookApp/src/main/java/com/chevbook/chevbookapp/Fragments/FragmentAbout.java
+++ ChevbookApp/src/main/java/com/chevbook/chevbookapp/Fragments/FragmentAbout.java
@@ -1,19 +1,17 @@
 package com.chevbook.chevbookapp.Fragments;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.astuetz.PagerSlidingTabStrip;
-import com.chevbook.chevbookapp.Adapter.ViewPagerAboutAdapter;
 import com.chevbook.chevbookapp.R;
+
+import butterknife.ButterKnife;
 
 
 public class FragmentAbout extends Fragment {
@@ -30,25 +28,13 @@ public class FragmentAbout extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        ButterKnife.inject(this, rootView);
 
         ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         String[] mNavigationTitles = getResources().getStringArray(R.array.navigation_array);
         actionBar.setTitle(mNavigationTitles[6]);
 
-        mTabsTitles = getResources().getStringArray(R.array.fragment_about_tab_array);
-
-        ViewPager mViewPager = (ViewPager) rootView.findViewById(R.id.viewPagerFragmentAbout);
-        mViewPager.setAdapter(new ViewPagerAboutAdapter(getChildFragmentManager(), mTabsTitles));
-
-        // Bind the tabs to the ViewPager
-        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) rootView.findViewById(R.id.pagerTabStripFragmentAbout);
-        tabs.setIndicatorColor(getResources().getColor(R.color.green_chevbook));
-        tabs.setTextColor(Color.WHITE);
-        tabs.setDividerColor(Color.WHITE);
-        tabs.setDrawingCacheEnabled(true);
-        tabs.setShouldExpand(true);
-        tabs.setViewPager(mViewPager);
 
         return rootView;
     }
